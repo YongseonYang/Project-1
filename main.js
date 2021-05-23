b35bcd3944c9c16d4cf24e527fe764e8019b8d35
YongseonYang <84008601+YongseonYang@users.noreply.github.com>
2021-05-23 17:08:56 +0900

    Project-1

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ toogleBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
     icons.classList.toggle('active')
 });
+
+$(".hello").click(function() {
+    var element = $(".conf");
+    blink(element);
+    setTimeout(function() {
+      reset(element);
+    }, 2000);
+  });
+  function blink(element, callback) {
+    element.css('visibility', 'visible');
+    element.css('opacity', '0');
+  }
+  function reset(element) {
+    element.css('visibility', 'hidden');
+    element.css('opacity', '1');
+  }
