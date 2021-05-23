7e447a9897fb514f3454a4e5f2a3619d2a11540a
YongseonYang <84008601+YongseonYang@users.noreply.github.com>
2021-05-23 17:53:46 +0900

    Project-1

--- main.js
+++ main.js
@@ -8,17 +8,23 @@ toogleBtn.addEventListener('click', () => {
 });
 
 $(".hello").click(function() {
-    var element = $(".conf");
-    blink(element);
-    setTimeout(function() {
-      reset(element);
-    }, 2000);
-  });
-  function blink(element, callback) {
-    element.css('visibility', 'visible');
-    element.css('opacity', '0');
-  }
-  function reset(element) {
-    element.css('visibility', 'hidden');
-    element.css('opacity', '1');
-  }
+  var element = $(".conf");
+  blink(element);
+  setTimeout(function() {
+    reset(element);
+  }, 2000);
+});
+function blink(element, callback) {
+  element.css('visibility', 'visible');
+  element.css('opacity', '0');
+}
+function reset(element) {
+  element.css('visibility', 'hidden');
+  element.css('opacity', '1');
+}
+
+if (el == "#stickyNav") {
+	el.find("h3").on('click', function(e) {
+		e.preventDefault();
+	});
+}
